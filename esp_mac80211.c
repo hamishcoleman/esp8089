bb04dcdf5b51dc3abbd40be16e6c73f07edcd8f2
Icenowy Zheng <icenowy@outlook.com>
2016-06-13 15:52:19 +0800

    esp_mac80211: said to remove a warning (seems useless)

--- esp_mac80211.c
+++ esp_mac80211.c
@@ -1202,7 +1202,7 @@ static int esp_op_ampdu_action(struct ieee80211_hw *hw,
 			       struct ieee80211_vif *vif,
 			       enum ieee80211_ampdu_mlme_action action,
 			       struct ieee80211_sta *sta, u16 tid,
-			       u16 * ssn, u8 buf_size)
+			       u16 * ssn, u8 buf_size, bool amsdu)
 {
 	int ret = -EOPNOTSUPP;
 	struct esp_pub *epub = (struct esp_pub *) hw->priv;
